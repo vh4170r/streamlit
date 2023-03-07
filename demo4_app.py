8c34fa1b1641f7d9bb415214b76816c1ba5754c4
vh4170r <vh4170r@gmail.com>
2023-03-07 21:24:16 +0100

    Update demo4_app.py

--- demo4_app.py
+++ demo4_app.py
@@ -7,3 +7,12 @@ option = st.selectbox(
      ('Blue', 'Red', 'Green'))
 
 st.write('Your favorite color is ', option)
+
+st.header('st.multiselect')
+
+options = st.multiselect(
+     'What are your favorite colors',
+     ['Green', 'Yellow', 'Red', 'Blue'],
+     ['Yellow', 'Red'])
+
+st.write('You selected:', options)
