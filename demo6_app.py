3e3b413eefc28126a0f629284878dfb355b39002
vh4170r <vh4170r@gmail.com>
2023-03-07 21:45:49 +0100

    Update demo6_app.py

--- demo6_app.py
+++ demo6_app.py
@@ -1,5 +1,5 @@
-import streamlit as st 
-import pandas as pd 
+import streamlit as st
+import pandas as pd
 import pandas_profiling
 from streamlit_pandas_profiling import st_profile_report
 
@@ -7,5 +7,5 @@ st.header('`streamlit_pandas_profiling`')
 
 df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
 
-pr = df.st_profile_report()
+pr = df.profile_report()
 st_profile_report(pr)
