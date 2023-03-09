3a5f03aa656ccb039ef08ff4aacc0a7882646ed3
vh4170r <vh4170r@gmail.com>
2023-03-09 20:28:08 +0100

    Update demo11_app.py

--- demo11_app.py
+++ demo11_app.py
@@ -1,19 +1,21 @@
-import streamlit as st 
+import streamlit as st
+
 st.set_page_config(layout="wide")
 
 st.title('How to layout your Streamlit app')
 
 with st.expander('About this app'):
-    st.write('This app shows the various ways on how you can layout your Streamlit app.')
-    st.image('https://streamlit.io/images/brand/streamlit-logo-secondary-colormark-darktext.png', width=250)
+  st.write('This app shows the various ways on how you can layout your Streamlit app.')
+  st.image('https://streamlit.io/images/brand/streamlit-logo-secondary-colormark-darktext.png', width=250)
 
 st.sidebar.header('Input')
+user_name = st.sidebar.text_input('What is your name?')
 user_emoji = st.sidebar.selectbox('Choose an emoji', ['', '😄', '😆', '😊', '😍', '😴', '😕', '😱'])
 user_food = st.sidebar.selectbox('What is your favorite food?', ['', 'Tom Yum Kung', 'Burrito', 'Lasagna', 'Hamburger', 'Pizza'])
 
 st.header('Output')
 
-col1, col2 , col3 = st.columns(3)
+col1, col2, col3 = st.columns(3)
 
 with col1:
   if user_name != '':
